314df5e5558b3f124bca9b2863161039b5234fec
Filip <filipjamroz21@gmail.com>
2024-10-28 19:48:38 +0000

    Comment and refactor Shooter.cs

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    // empty game object with Guns as children
     [SerializeField] GameObject _gunHolder;
-    List<Gun> _guns;
 
+    List<Gun> _guns;
     GameplayAudioPlayer _audioPlayer;
 
+    // PowerManager equips/unequips guns using this property
     public GameObject GunHolder { get => _gunHolder; set => _gunHolder = value; }
 
     void Awake() {
         _audioPlayer = FindObjectOfType<GameplayAudioPlayer>();
-        UpdateWeapons();
+        PrepareWeapons();
     }
 
-    public void UpdateWeapons() {
+    // prepares guns for shooting
+    public void PrepareWeapons() {
+        // gets Gun component from children into a list for easy access
         _guns = _gunHolder.GetComponentsInChildren<Gun>().ToList();
     }
     public void FireWeapons() {
         if(_guns == null) { return; }
+
+        // shoot every gun
         _guns.ForEach(gun => gun.Shoot());
         _audioPlayer.PlayShootingClip();
     }
