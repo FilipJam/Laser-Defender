03bd466a3b49258fb3c5583fe2e35446cd39095c
Filip <filipjamroz21@gmail.com>
2024-10-28 19:32:03 +0000

    Comment EnemyShootController.cs

--- Assets/Scripts/EnemyShootController.cs
+++ Assets/Scripts/EnemyShootController.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 public class EnemyShootController : MonoBehaviour
 {
     [SerializeField] float _shootCooldown = 1f;
-
     [SerializeField] float _shootVariance = 0.5f;
     [SerializeField] float _minimumShootCooldown = 0.2f;
 
-    [SerializeField] bool _canShoot = true;
     Shooter _shooter;
 
-    void Awake() => _shooter = GetComponent<Shooter>();
+    void Awake() {
+        _shooter = GetComponent<Shooter>();
+    }
 
-    void Start() => StartCoroutine(Shoot());
-
+    void Start() {
+        StartCoroutine(Shoot());
+    }
 
     IEnumerator Shoot() {
-        while(_canShoot) {
+        // endless shooting
+        while(true) {
             _shooter.FireWeapons();
+            // fire lasers at different times
             yield return new WaitForSeconds(GetRandomShootCooldown());
-            
         }
     }
 
     float GetRandomShootCooldown() {
+        // calculate minimum and maximum spawn time
         float min = _shootCooldown - _shootVariance;
         float max = _shootCooldown + _shootVariance;
 
         float randomCooldown = Random.Range(min, max);
+        // constrains spawn time
         return Mathf.Clamp(randomCooldown, _minimumShootCooldown, max);
     }
 }
